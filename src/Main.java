de35340adedcbfd4d9f169b181e4fc33682a57d4
Андрей <honor10love2@gmail.com>
2022-03-23 23:44:23 +0300

    порешали почти все до середины 2

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,43 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         printMenu();
         int userInput = scanner.nextInt();
-
+        StepTracker stepTracker = new StepTracker();
         while (userInput != 0) {
             // обаботка разных случаев
+            if (userInput == 1) {
+                System.out.println("Введите месяц: (По русски, пример: 'Январь')");
+                scanner.nextLine();
+                String month = scanner.nextLine();
+                System.out.println("Введите день:");
+                int day = scanner.nextInt();
+                System.out.println("Введите количество шагов:");
+                int numberOfStep = scanner.nextInt();// ввести количество шагов за определенный день
+                stepTracker.saveStepNumber(month, day, numberOfStep);
+            } else if (userInput == 2) {
+                scanner.nextLine();
+                String month = scanner.nextLine();
+                stepTracker.statistics(month); // напечатать статистику за определенный месяц
+            } else if (userInput == 3) {
+                // изменить цель по количеству шагов вдень
+            } else {
+                System.out.println("Такой команды нет!");
 
+            }
             printMenu(); // печатем меню ещё раз перед завершением предыдущего действия
             userInput = scanner.nextInt(); // повторное считывание данных от пользователя
-    }
+        }
         System.out.println("Программа завершена");
-}
+    }
+
     private static void printMenu() {
-        while (true) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Что выберете?");
-            System.out.println("1 - Ввести количество шагов за определённый день");
-            System.out.println("2 - Напечатать статистику за определённый месяц");
-            System.out.println("3 - Изменить цель по количеству шагов в день");
-            System.out.println("0 - Выйти из приложения.");
-            int command = scanner.nextInt();
-
-            if (command == 1) {
-
-                Scanner scanner = new Scanner(System.in);
-                String stepOfDay = scanner.nextLine();
-
-
-
-            } else if (command == 2) {
+        System.out.println("Что выберете?");
+        System.out.println("1 - Ввести количество шагов за определённый день");
+        System.out.println("2 - Напечатать статистику за определённый месяц");
+        System.out.println("3 - Изменить цель по количеству шагов в день");
+        System.out.println("0 - Выйти из приложения.");
     }
 }
--- src/StepTracker.java
+++ src/StepTracker.java
@@ -1,21 +1,81 @@
-import java.util.Scanner;
-import java.util.HashMap;
 import java.util.ArrayList;
+import java.util.HashMap;
+
 public class StepTracker {
+    int goalOfStep = 10000;
     HashMap<Integer, MonthData> monthToData = new HashMap<Integer, MonthData>();
+    HashMap<String, Integer> monthAndValues = new HashMap<>();
 
     public StepTracker() {
         for (int i = 0; i < 12; i++) {
             monthToData.put(i, new MonthData());
         }
+        monthAndValues.put("Январь", 0);
+        monthAndValues.put("Февраль", 1);
+        monthAndValues.put("Март", 2);
+        monthAndValues.put("Апрель", 3);
+        monthAndValues.put("Май", 4);
+        monthAndValues.put("Июнь", 5);
+        monthAndValues.put("Июль", 6);
+        monthAndValues.put("Август", 7);
+        monthAndValues.put("Сентябрь", 8);
+        monthAndValues.put("Октябрь", 9);
+        monthAndValues.put("Ноябрь", 10);
+        monthAndValues.put("Декабрь", 11);
+
     }
 
     class MonthData {
+        HashMap<Integer, Integer> monthData = new HashMap<Integer, Integer>();
 
-        HashMap<String, Integer> stepOfDay = new HashMap<String, Integer>();
-       for (int i = 1; i < 31; i++){
-            stepOfDay.put(i,)
-        } // Заполните класс самостоятельно
+        public MonthData() {
+
+            for (int i = 1; i < 31; i++) {
+                monthData.put(i, 0);
+            }
+        }
+    }
+
+    public void saveStepNumber(String month, int day, int numberOfStep) {
+        if (!monthAndValues.containsKey(month)) {
+            System.out.println("Такого месяца нет!");
+            return;
+        }
+        Integer numberMonth = monthAndValues.get(month);
+        MonthData DayAndStep = monthToData.get(numberMonth);
+        if (!DayAndStep.monthData.containsKey(day)) {
+            System.out.println("Такого дня нет!");
+            return;
+        }
+        if (numberOfStep < 0) {
+            System.out.println("Шаги не могут быть отрицательными!");
+            return;
+        }
+        DayAndStep.monthData.put(day, numberOfStep);
+    }
+
+    public void statistics(String month) {
+        if (!monthAndValues.containsKey(month)) {
+            System.out.println("Такого месяца нет!");
+            return;
+        }
+        Integer numberMonth = monthAndValues.get(month);
+        MonthData DayAndStep = monthToData.get(numberMonth);
+        ArrayList<String> day = new ArrayList<>();
+
+        for (Integer key : DayAndStep.monthData.keySet()) {
+            Integer values = DayAndStep.monthData.get(key);
+            day.add(key + " день: " + values);
+
+
+        }
+
+        System.out.println(String.join(", ", day));
+        
+    }
+
+    public void goalStep() {
+
     }
 
 }
